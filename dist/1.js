70303d19ea9fd1797d19fe7c694b25fdeeb192ff
Danny <danny@shoppimon.com>
2020-02-03 15:51:24 +0200

    new build with bike-app

---
+++ dist/1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{3:function(n,e){e.bootstrap=function(){return Promise.resolve()},e.mount=function(){return new Promise(n=>{const e=new XMLHttpRequest;e.open("GET","/page1.html"),e.send(),e.onreadystatechange=function(){4===e.readyState&&n(e.responseText)}}).then(n=>document.getElementById("content").innerHTML=n)},e.unmount=function(){return Promise.resolve().then(()=>document.getElementById("content").innerHTML="")}}}]);
+//# sourceMappingURL=1.js.map
